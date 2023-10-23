45953cd47952a3c384eb443eeb8f06892b1f7b9d
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-10-23 10:48:40 +0100

    some small structural changes, updated plan on figma

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Footer() {
+    
+
+  return (
+    <footer>
+        Some information in the footer
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Header() {
+    
+
+  return (
+    <header>
+        <h1>
+            NC NEWS
+        </h1>
+    </header>
+  )
+}
+
+export default Header
