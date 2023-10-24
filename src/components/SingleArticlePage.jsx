19b0b44af3317a8c5be4678505e21bd64e757bb7
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-10-24 09:33:07 +0100

    added comments to single article page

--- src/components/SingleArticlePage.jsx
+++ src/components/SingleArticlePage.jsx
@@ -5,30 +5,53 @@ import { useParams } from 'react-router-dom'
 function SingleArticlePage() {
 
 const [currentArticle, setCurrentArticle] = useState({})
+const [comments, setComments] = useState([])
 
 useEffect(()=> {
-    fetchArticles().then((response) => 
-
-    response.json()).then(({article}) => {
+    fetchArticle().then((response) => response.json()).then(({article}) => {
 
         setCurrentArticle(article)
     })
+
+    fetchComments().then((response)=> response.json()).then(({comments}) => {
+
+      setComments(comments)
+  })
+
+    
 }, [])
 
 
 const {articleid} = useParams()
-const fetchArticles = () => {
+const fetchArticle = () => {
     return fetch(`https://nc-news-z0zw.onrender.com/api/articles/${articleid}`)
     }
+const fetchComments = () => {
+  return fetch(`https://nc-news-z0zw.onrender.com/api/articles/${articleid}/comments`)
+  }
+
   return (
     <>
-    {/* SOME HREF ELEMENT HERE TO JUMP TO COMMENTS SECTION */}
     <h3>{currentArticle.title}</h3>
-    <p>Posted at: {currentArticle.created_at} Votes: {currentArticle.votes} Comments: {currentArticle.comment_count}</p>
+    <p>Posted at: {currentArticle.created_at} by {currentArticle.author}</p> <p>Votes: {currentArticle.votes}</p> <a href="#comments">Comments: {currentArticle.comment_count}</a>
     <img src={currentArticle.article_img_url} />
     <p>
     {currentArticle.body}
-      </p></>
+      </p>
+
+    <h3>Comments</h3>
+    <section className="comments-section" id="comments">
+    {comments.map((comment) => {
+              return (
+                  <div key = {comment.article_id} className="single-comment">
+                    <p>Posted at: {comment.created_at} by {comment.author}</p> <p>Votes: {comment.votes}</p> <p></p>
+                    <p>{comment.body}</p>
+                  </div>
+                  
+              )
+          })} 
+          </section>
+      </>
   )
 }
 
